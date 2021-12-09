a8c66fe93512f651899cb9fe36036239830a9f56
IvanArjonaAmadorUPM <ivan.arjona.amador@alumnos.upm.es>
2021-12-09 19:43:19 +0100

    Actualizando los mock comunes

--- src/test/java/com/practica/integracion/TestInvalidUser.java
+++ src/test/java/com/practica/integracion/TestInvalidUser.java
@@ -2,10 +2,16 @@ package com.practica.integracion;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 @ExtendWith(MockitoExtension.class)
 public class TestInvalidUser {
 	/**
 	 * RELLENAR POR EL ALUMNO
 	 */
+	@Mock
+	private static AuthDAO mockAuthDao;
+	@Mock
+	private static GenericDAO mockGenericDao;
 }
