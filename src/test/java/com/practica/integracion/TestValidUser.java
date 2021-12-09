5ffc50ad9354e6653b7a6a465db17d4bab96222f
Santiago <S.tmarco@alumnos.upm.es>
2021-12-09 19:56:13 +0100

    espacio Santi

--- src/test/java/com/practica/integracion/TestValidUser.java
+++ src/test/java/com/practica/integracion/TestValidUser.java
@@ -65,4 +65,11 @@ public class TestValidUser {
 	public void testAddRemoteSystemWithValidUserAndSystem() throws Exception{
 
 	}
+
+
+	//-------------------------Espacio Santiago-----------------------------------
+	@Test
+	public void teststopRemoteSystemWithValidUserAndSystem() throws Exception{
+
+	}
 }
